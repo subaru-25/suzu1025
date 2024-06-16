f286bfb14a0b1643491e6a172f9c881e92192d45
subaru-25 <misuzu101025@gmail.com>
2024-06-16 18:33:43 +0900

    HTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  get 'prototypes/index'
-  root to: "prototypes#index"
+  get 'users/new'
+  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root 'prototypes#index' # ホームページのルート
+
+  resources :recipes
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
 end
